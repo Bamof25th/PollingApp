a4ff1f7694a7ce66ff7b311e199105b6a57e3a74
Aniket Baghel <88363603+Bamof25th@users.noreply.github.com>
2025-06-28 23:47:28 +0530

    Implement authentication features with JWT, including AuthController, AuthService, and AuthServiceImpl; update security configuration and CORS settings; modify User and Role repositories.

---
+++ polling/src/main/java/com/learn/polling/domain/dtos/UserDto.java
@@ -0,0 +1,15 @@
+package com.learn.polling.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class UserDto {
+    private Long id;
+    private String name;
+    private String userName;
+    private String email;
+}
